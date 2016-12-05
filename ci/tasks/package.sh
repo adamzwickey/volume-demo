e2e79f9813ca82339c6581a088851f94eaa8e72a
azwickey-pivotal <vania12R>
2016-12-05 14:33:10 -0500

    Added Concourse Pipeline

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,6 +2,10 @@
 
 set -xe
 
+VERSION=$(<version/number)
+
 cd git-assets/
 mvn -DskipTests=true package
+
+echo "saving file with version number: $VERSION"
 cp target/*.jar ../volume-demo-releases/volume-demo-$VERSION.jar
