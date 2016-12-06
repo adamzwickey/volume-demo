961361b95e217b6057ce2b69beed40cb0f12af1f
azwickey-pivotal <vania12R>
2016-12-05 22:58:27 -0500

    Added Concourse Pipeline

--- ci/tasks/int-test.sh
+++ ci/tasks/int-test.sh
@@ -7,11 +7,5 @@ export NEXT_APP_URL=http://$CF_APP-$NEXT_APP_COLOR.$CF_APP_DOMAIN/
 
 echo "Running curl integration tests..."
 
-curl --include -k $NEXT_APP_URL | grep "HTTP/1.1 200 OK" > result.txt
-export RESULT=$(cat result.txt)
-if [[ $RESULT =~ .*OK.* ]]
-then
-  return 0
-else
-  return 1
-fi
+curl --include -k $NEXT_APP_URL | grep "HTTP/1.1 200 OK"
+
