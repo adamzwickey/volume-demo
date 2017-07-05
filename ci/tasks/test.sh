29713cce799b955f9124696e2ba3e00b82ed7974
azwickey-pivotal <vania12R>
2017-07-05 11:29:21 -0400

    update test script

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 set -xe
-. ~/.bashrc
-whoami
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
+
 echo $PATH
 java -version
 mvn -v
