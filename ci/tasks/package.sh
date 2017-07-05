936ded7d73b9258b81c87f2f58676b5a5afbbb8a
azwickey-pivotal <vania12R>
2017-07-05 11:31:35 -0400

    update test script

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,6 +4,14 @@ set -xe
 
 VERSION=$(<version/number)
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+export M2_HOME=/usr/local/apache-maven/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
+
 cd git-assets/
 mvn -DskipTests=true package
 
